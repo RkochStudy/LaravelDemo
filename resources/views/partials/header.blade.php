95865261b56aace8e6010e0bd515704bfaa48ded
Sergio S <theLastRkoch@gmail.com>
2018-11-29 18:27:01 -0600

    Add the work in post method

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,9 @@
             <a class="navbar-brand" href="{{route('blog.index')}}">Laravel Guilde</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
             <li><a class="nav-link" href="{{route('blog.index')}}">Blog</a></li>
             <li><a class="nav-link" href="{{route('other.about')}}">About</a></li>
+            <li><a class="nav-link" href="{{route('admin.index')}}">Admin</a></li>
         </ul>
     </div>
 </nav>
