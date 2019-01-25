f7806d9cc93ea25e60c7c93997c87b8aaffd98bc
Sergio S <rkoch.study@outlook.com>
2019-01-25 10:19:27 -0600

    Added the reset password functionality

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Notifications\CustomPasswordReset;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -30,5 +31,10 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany('App\Post');
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomPasswordReset($token));
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('no-replay@test.com', 'hello@example.com'),
+        'name' => env('Sergio S.', 'Example'),
     ],
 
     /*
